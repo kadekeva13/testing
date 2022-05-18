cf540bc4946fa0cf0e80eee7ff7fa80f7afd9c6f
kadekeva13 <devgram89@gmail.com>
2022-05-18 18:29:33 +0700

    Kategori dan penambahan suara dan ada error di manifest

--- app/src/main/java/com/example/myapplication/Ping.java
+++ app/src/main/java/com/example/myapplication/Ping.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class test extends AppCompatActivity {
+public class Ping extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_ping);
     }
 }
